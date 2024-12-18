43f102b62bd00722f9920a24c9c431cd306903fa
Vani Chaturvedi <workwithvanichaturvedi@gmail.com>
2024-12-18 22:25:25 +0530

    Updated code with requested changes

--- asg0_1.c
+++ asg0_1.c
@@ -1,123 +1,123 @@
 #include <stdio.h>
 #include <limits.h>
-int number(char s){
-    if(s<='9'&&s>='0'){
+int isNumber(char currChar){
+    if(currChar<='9'&&currChar>='0'){
         return 1;
     }
     return 0;
+    
 }
 
-int space(char s){
-    if(s==' '){
+int isSpace(char currChar){
+    if(currChar==' '){
         return 1;
     }
     return 0;
+
 }
 
-int gen_num(char* s, int* i) {
-    int x = 0;
-    while (number(s[*i])==1) {
-        x = x * 10 + (s[*i] - '0');
-        (*i)++;
+int checkOperator(char currChar){
+    if(currChar == '+' || currChar== '-' || currChar == '*' || currChar == '/'){
+        return 1;
     }
-    return x;
+    return 0;
+
 }
 
-int soln(char* s) {
-    int nums[10000] = {0};
-    char op_sign[10000];
-    int err1_code= INT_MAX, err2_code = INT_MIN;
-    int num_p = 0;
-    int sign_p = 0;
-    int i = 0,j=0;
-    while(s[j]){
-        if(space(s[j])==1) j++;
-        else if(number(s[j])==1) j++;
-        else if(s[j]=='+'||s[j]=='-'||s[j]=='*'||s[j]=='/') j++;
-        else return err1_code;
+int getNumber(char* currChar, int* index) {
+    int tempNumber = 0;
+    while (isNumber(currChar[*index])==1) {
+        tempNumber = tempNumber * 10 + (currChar[*index] - '0');
+        (*index)++;
     }
+    return tempNumber;
 
-    while (s[i]) {
-        if (space(s[i])==1) i++;
-        else if (number(s[i])==1) {
-            int x = gen_num(s, &i); 
-            nums[num_p] = x;
-            num_p++;
-        } else { // if +- then move to sign array else solve the *&/
-            if (s[i] == '*' || s[i] == '/') {
-                char curr_operator = s[i];
-                i++;
-                while (space(s[i])==1)
-                    i++;
+}
 
-                int operand_2 = gen_num(s, &i); 
-
-                int operand_1 = nums[--num_p];
-
-                if (curr_operator == '*') {
-                    nums[num_p] = operand_1 * operand_2;
-                    num_p++;
-                } else {
-                    if(operand_2==0){
-                        printf("Error division by zero");
-                        return err2_code;
-                    }
-                    nums[num_p] = operand_1 / operand_2;
-                    num_p++;
-                }
-            } else {
-                op_sign[sign_p] = s[i];
-                i++;
-                sign_p++;
-            }
-        }
-    }
-
-    int ans = nums[0];
+int evaluateResult(int operands[],char op_sign[], int sign_p){
+    int ans = operands[0];
     int index = 1; 
     for (int i = 0; i < sign_p; i++) {
         if (op_sign[i] == '+') {
-            ans += nums[index];
+            ans += operands[index];
         } else {
-            ans -= nums[index];
+            ans -= operands[index];
         }
         index++;
     }
     return ans;
+
 }
+
+int calculate(char* inputExp) {
+    int operands[10000] = {0}, invalidInputError = INT_MAX, divByZero = INT_MIN, numIndex = 0, operandInd = 0, index = 0, inputIndex=0;
+    char operators[10000];
+
+    while(inputExp[inputIndex]){
+        if(isSpace(inputExp[inputIndex])==1 || isNumber(inputExp[inputIndex])==1 || checkOperator(inputExp[inputIndex])) inputIndex++;
+        else return invalidInputError;
+    }
+
+    while (inputExp[index]) {
+        if (isSpace(inputExp[index])==1) index++;
+        else if (isNumber(inputExp[index])==1) {
+            operands[numIndex++] = getNumber(inputExp, &index);
+        } else { 
+            if (inputExp[index] == '*' || inputExp[index] == '/') {
+                char curr_operator = inputExp[index];
+                index++;
+                while (isSpace(inputExp[index])==1) index++;
+                int operand_2 = getNumber(inputExp, &index); 
+                int operand_1 = operands[--numIndex];
+                if (curr_operator == '*') {
+                    operands[numIndex++] = operand_1 * operand_2;
+                } else {
+                    if(operand_2==0){
+                        return divByZero;
+                    }
+                    operands[numIndex++] = operand_1 / operand_2;
+                }
+            } else {
+                operators[operandInd++] = inputExp[index++];
+            }
+        }
+    }
+    int result = evaluateResult(operands,operators,operandInd);
+    return result;
+
+}
+
 int main() {
-    char str[10000];
-    int i = 0;
-    char ch;
+    char inputExp[10000];
+    int index = 0;
+    char inputChar;
 
     printf("Enter a string: ");
-    
-    do{
-        ch=getchar();
-        if(ch!='\n'){
-            str[i]=ch;
-            i++;
-        }
-    }while(ch!='\n');
+    inputChar=getchar();
+    while(inputChar!='\n'){
+        inputExp[index]=inputChar;
+        index++;
+        inputChar=getchar();
+    }
 
-    str[i] = '\0';
-    int result = soln(str);
+    inputExp[index] = '\0';
+    int result = calculate(inputExp);
 
-    if(str[0] == '\0'){
+    if(inputExp[0] == '\0'){
         printf("The input string is empty");
-        return 0;
     }
     else if(result==INT_MAX){ 
         printf("Invalid character in the string"); 
-        return 0;
     }
 
     else if(result==INT_MIN){
-        return 0;
+        printf("Error division by zero");
     }
 
     else {
         printf("Result: %d\n", result);
-        return 0;
     }
+    
+    return 0;
+
 }
